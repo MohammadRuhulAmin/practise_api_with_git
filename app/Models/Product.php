2e847f3aabd673fdbd2a5de854dd777821dfe422
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2020-12-18 11:57:43 +0600

    Relation Between Product and Review Model is Created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Review;
 class Product extends Model
 {
-    //
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
