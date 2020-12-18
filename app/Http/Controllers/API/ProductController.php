9cf8805a044f5b30e9bfdb8b91cc1af5e93075fe
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2020-12-18 13:02:39 +0600

    Resource Controller ProductController and API is registered and Crud Operation Created

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $product = Product::all();
+        return $product;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product();
+        $product->name=$request->name;
+        $product->detail = $request->detail;
+        $product->stock = $request->stock;
+        $product->discourt = $request->discount;
+        $product->prize = $request->prize;
+        $product->save();
+        return "Product is inserted successfully!";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return $product;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->name=$request->name;
+        $product->detail = $request->detail;
+        $product->stock = $request->stock;
+        $product->discount = $request->discount;
+        $product->prize = $request->prize;
+        $product->save();
+        return "Product is Updated Successfully!";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $isDeleted =  Product::destroy($id);
+        if($isDeleted)return "Deleted Successfully!";
+        else return "Not Deleted!";
+    }
+}
